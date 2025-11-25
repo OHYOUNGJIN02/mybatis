17e8fcde2f6bf7f1a106f23a4287d339faf2cfac
gimbumbucu <tgimbumbucu@gmail.com>
2025-11-25 11:14:30 +0900

    1

--- src/main/java/com/korit/mybatis_study/Controller/TodoController.java
+++ src/main/java/com/korit/mybatis_study/Controller/TodoController.java
@@ -1,6 +1,7 @@
 package com.korit.mybatis_study.Controller;
 
 import com.korit.mybatis_study.Dto.AddTodoReqDto;
+import com.korit.mybatis_study.Dto.EditTodoReqDto;
 import com.korit.mybatis_study.Service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -28,9 +29,20 @@ public class TodoController {
     }
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<?> getTodoListByBoardId(@PathVariable Integer todoId){
+    public ResponseEntity<?> findTodoByTodoId(@PathVariable Integer todoId){
         return ResponseEntity.ok(todoService.findTodoByTodoId(todoId));
     }
+
+    @PostMapping("/edit")
+    public ResponseEntity<?> updateTodo(@RequestBody EditTodoReqDto editTodoReqDto){
+        return ResponseEntity.ok(todoService.editTodo(editTodoReqDto));
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity<?> removeTodo(@RequestParam Integer todoId){
+        return ResponseEntity.ok(todoService.removeTodo(todoId));
+    }
+    //12321
 
 
 }
--- src/main/java/com/korit/mybatis_study/Service/TodoService.java
+++ src/main/java/com/korit/mybatis_study/Service/TodoService.java
@@ -3,6 +3,8 @@ package com.korit.mybatis_study.Service;
 import com.korit.mybatis_study.Dto.AddTodoReqDto;
 import com.korit.mybatis_study.Dto.ApiDataRespDto;
 import com.korit.mybatis_study.Dto.ApiRespDto;
+import com.korit.mybatis_study.Dto.EditTodoReqDto;
+import com.korit.mybatis_study.Entity.Board;
 import com.korit.mybatis_study.Entity.Todo;
 import com.korit.mybatis_study.Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +20,9 @@ public class TodoService {
     private TodoRepository todoRepository;
 
     public ApiRespDto addTodo(AddTodoReqDto addTodoReqDto) {
-        Optional<Todo> foundUsername = todoRepository.findByTitle(addTodoReqDto.getTitle());
-        if (foundUsername.isPresent()){
-            return new ApiRespDto("failed", "유저 네임이 중복되었습니다", null);
+        Optional<Todo> foundTodo = todoRepository.findByTitle(addTodoReqDto.getTitle());
+        if (foundTodo.isPresent()){
+            return new ApiRespDto("failed", "제목이 중복되었습니다", null);
 
         }
         Optional<Todo> todo = todoRepository.addTodo(addTodoReqDto.toEntity());
@@ -40,10 +42,27 @@ public class TodoService {
         }
         return new ApiRespDto<>("success", "조회 완료", foundTodo.get());
     }
-    public ApiRespDto<?>editTodo(Integer todoId){
-        Optional<Todo> foundTodo = todoRepository.findTodoByTodoId(todoId);
+    public ApiRespDto<?>editTodo(EditTodoReqDto editTodoReqDto) {
+        Optional<Todo> foundTodo = todoRepository.findTodoByTodoId(editTodoReqDto.getTodoId());
         if (foundTodo.isEmpty()){
-            return new ApiRespDto<>("failed", "수정 실패",  null);
+            return new ApiRespDto<>("failed", "실패",  null);
         }
+        int result = todoRepository.editTodo(editTodoReqDto.toEntity());
+        if (result != 1) {
+            return new ApiRespDto<>("failed", "수정하는데 문제가 발생했습니다.", null);
+        }
+        return new ApiRespDto<>("success", "수정 성공", null);
     }
-}
+    public ApiRespDto<?> removeTodo(Integer todoId) {
+        Optional<Todo> foundTodo = todoRepository.findTodoByTodoId(todoId);
+        if (foundTodo.isEmpty()) {
+            return new ApiRespDto<>("failed", "해당 존재하지 않습니다.", null);
+        }
+        int result = todoRepository.removeTodo(foundTodo.get().getTodoId());
+        if (result != 1) {
+            return new ApiRespDto<>("failed", "삭제하는데 문제가 발생했습니다.", null);
+        }
+        return new ApiRespDto<>("success", "삭제되었습니다.", null);
+    }
+
+    }
